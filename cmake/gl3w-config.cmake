c17826f980cf49b523adc6fab5260e0fee0c9587
Konstantin Podsvirov <konstantin@podsvirov.pro>
2018-07-03 23:38:43 +0300

    Add install rules

--- cmake/gl3w-config.cmake
+++ cmake/gl3w-config.cmake
@@ -1,1 +1,6 @@
+if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.8.0)
+	include(CMakeFindDependencyMacro)
+	find_dependency(OpenGL REQUIRED)
+endif()
+
 include("${CMAKE_CURRENT_LIST_DIR}/gl3w-targets.cmake")
